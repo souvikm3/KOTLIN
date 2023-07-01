5137b1f48e7cdc1697e055073a9b154b89820d93
Souvik Maiti <souvikmaiti391@gmail.com>
2023-07-01 10:11:26 +0530

    Basic Update

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -1,3 +1,14 @@
 fun main(){
     //Declare variable
+    var strType="KOTLIN"
+    val salary=10000
+    //KOTLIN Data Types
+    var num: Int=10;
+    var d:Double=100.10
+    var e:Float=10.10f
+    var f:Long=100000005
+    println("The Integer value is: "+ num)
+    println("The Double value is: "+ d)
+    println("The Float value is: "+ e)
+    println("The Long value is: "+ f)
 }
