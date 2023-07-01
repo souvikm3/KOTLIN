95842005334d694d68fe0abe028c12cad15c43d9
Souvik Maiti <souvikmaiti391@gmail.com>
2023-07-01 10:32:48 +0530

    Operator Update

---
+++ src/main/kotlin/char.kt
@@ -0,0 +1,18 @@
+fun main(){
+    //Defining character
+    val newChar: Char='S'
+    println(newChar)
+    val Bool:Boolean=true
+    println(Bool)
+    val str:String="I am Souvik"
+    println(str)
+    //Kotlin operator
+    //Arithmatic operator(+,-,/,*,%)
+    val a=10
+    val b=5
+    println(a+b)
+    println(a-b)
+    println(a/b)
+    println(a*b)
+    println(a%b)
+}
